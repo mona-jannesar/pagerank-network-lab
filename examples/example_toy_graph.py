877d077d134bcf284825189f23115fed71d6db52
Mona Jannesar <mona.jannesar@gmail.com>
2025-11-24 22:30:18 -0500

    fix sourcing of pagerank_lab librarires

--- examples/example_toy_graph.py
+++ examples/example_toy_graph.py
@@ -1,6 +1,11 @@
 # examples/example_toy_graph.py
+import sys, os
+print(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from pagerank_lab.graph import build_graph_from_edges
 from pagerank_lab.pagerank import pagerank
+
 
 if __name__ == "__main__":
     edges = [
